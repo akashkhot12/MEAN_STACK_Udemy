d7e5d4926518e74c8d1a732a43ff1146e9b114c1
akashkhot12 <akashkhot03@gmail.com>
2024-03-18 13:34:48 +0530

    adding new compoent

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations/index";
+// import { BrowserAnimationsModule } from "@angular/platform-browser/animations/index";
  import { FormsModule } from "@angular/forms";
 
 import { MatInputModule } from "@angular/material/input";
-
+import { MatCardModule } from "@angular/material/card";
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule
 
   ],
   providers: [
